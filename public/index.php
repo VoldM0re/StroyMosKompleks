c69cad76fc1ba5cdde55e54c6d655c1f1030fb47
unknown <dadasik89@gmail.com>
2025-03-11 01:21:26 +0300

    Изменена структура проекта

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>СтройМосКомплекс - Главная</title>
+    <meta name='description'
+        content='Строительство, проектирование и дизайн под ключ в Москве и области. Профессиональные услуги промышленного альпинизма. Гарантия качества и соблюдение сроков.'>
+    <link rel='stylesheet' href='../css/main.css'>
+    <link rel='shortcut icon' href='../src/svg/favicon.svg' type='image/x-icon'>
+</head>
+
+<body>
+    <?php require_once '../includes/components/header.php'; ?>
+    <div class='slider-wrapper container'>
+        <div class='slider'>
+            <div class='slider__item'>
+                <img src='../src/img/slider1.png' alt='Slider image 1'>
+            </div>
+        </div>
+    </div>
+
+    <script src='main.js'></script>
+</body>
+
+</html>
