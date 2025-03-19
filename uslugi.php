f59742430dd8a383601e03a36f1e00ee16194423
unknown <dadasik89@gmail.com>
2025-03-20 02:01:45 +0300

    Изображения разделены на каталоги по страницам, на которых они находятся

--- uslugi.php
+++ uslugi.php
@@ -21,29 +21,29 @@
             <a class="service" href="#">
                 <p class="service-text">Промышленный альпинизм</p>
                 <picture>
-                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi_PromAlp_desktop.webp">
-                    <img src="/assets/img/uslugi_PromAlp_mobile.webp" alt="Услуги" width="1080">
+                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi/PromAlp_desktop.webp">
+                    <img src="/assets/img/PromAlp_mobile.webp" alt="Услуги" width="1080">
                 </picture>
             </a>
             <a class="service" href="#">
                 <p class="service-text">Строительные работы</p>
                 <picture>
-                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi_Stroy_desktop.webp">
-                    <img src="/assets/img/uslugi_Stroy_mobile.webp" alt="Услуги" width="1080">
+                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi/Stroy_desktop.webp">
+                    <img src="/assets/img/Stroy_mobile.webp" alt="Услуги" width="1080">
                 </picture>
             </a>
             <a class="service" href="#">
                 <p class="service-text">Отделочные работы</p>
                 <picture>
-                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi_Otdelka_desktop.webp">
-                    <img src="/assets/img/uslugi_Otdelka_mobile.webp" alt="Услуги" width="1080">
+                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi/Otdelka_desktop.webp">
+                    <img src="/assets/img/Otdelka_mobile.webp" alt="Услуги" width="1080">
                 </picture>
             </a>
             <a class="service" href="#">
                 <p class="service-text">Дизайн и проектирование</p>
                 <picture>
-                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi_Proekt_desktop.webp">
-                    <img src="/assets/img/uslugi_Proekt_mobile.webp" alt="Услуги" width="1080">
+                    <source media="(min-width: 659px)" srcset="/assets/img/uslugi/Proekt_desktop.webp">
+                    <img src="/assets/img/Proekt_mobile.webp" alt="Услуги" width="1080">
                 </picture>
             </a>
         </div>
