775bcd0ae956f0accce99d33204495a7d8226cd8
unknown <dadasik89@gmail.com>
2025-03-23 21:26:46 +0300

    Добавлено мета-описание на страницу портфолио и исправлено на странице о компании

---
+++ portfolio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>СтройМосКомплекс - Портфолио</title>
+    <meta name='description'
+        content='Все наши выполненные работы на одной странице. Узнайте, какие задачи клиентов мы решаем и начните сотрудничество уже сегодня!'>
+    <link rel='stylesheet' href='/css/pages/o-kompanii.css'>
+    <link rel='shortcut icon' href='/assets/svg/favicon.svg' type='image/x-icon'>
+</head>
+
+<body>
+    <?php require_once 'includes/components/header.php'; ?>
+    <main>
+
+    </main>
+    <?php require_once 'includes/components/footer.php'; ?>
+</body>
+
+</html>
