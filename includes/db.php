fc6fbd6fd36205447d2b0d0519bceda5dd89ac28
VoldM0re <dadasik89@gmail.com>
2025-04-29 03:38:59 +0300

    fix: Исправлены пути и добавлены игнорируемые файлы
    - Исправлен путь к файлу config.php в db.php
    - Добавлена директория для загруженных аватарок в .gitignore
    - Добавлено исключение для файла default_pfp.png в .gitignore

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    require_once $_SERVER['DOCUMENT_ROOT'] . 'config.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
     $pdo = new PDO("mysql:host={$config['host']};dbname={$config['name']};charset={$config['charset']}", $config['user'], $config['pwd']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
