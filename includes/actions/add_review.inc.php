be38db5394e835f0ec68e5a91348af16468538da
unknown <dadasik89@gmail.com>
2025-05-03 19:14:55 +0300

    feat: Добавление уведомлений об отправке отзыва
    - В add_review.inc.php добавлена обработка ошибок при добавлении отзыва в БД и запись в сессию уведомления об успехе/ошибке
    - На странице otzyvy.php добавлено отображение уведомления ошибки/успеха при отправке отзыва на модерацию
    - Добавлен новый css-компонент message.css

--- includes/actions/add_review.inc.php
+++ includes/actions/add_review.inc.php
@@ -6,8 +6,14 @@ require_once '../db.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $review_text = getPost('review_text');
 
-    $stmt = $pdo->prepare("INSERT INTO `reviews` (`user_id`, `review_text`) VALUES (:user_id, :review_text);");
-    $stmt->execute([':review_text' => $review_text, ':user_id' => $_SESSION['user']['id']]);
+    try {
+        $stmt = $pdo->prepare("INSERT INTO `reviews` (`user_id`, `review_text`) VALUES (:user_id, :review_text);");
+        $stmt->execute([':review_text' => $review_text, ':user_id' => $_SESSION['user']['id']]);
+
+        $_SESSION['message'] = ['type' => 'success', 'text' => 'Ваш отзыв отправлен на модерацию'];
+    } catch (\Throwable $th) {
+        $_SESSION['message'] = ['type' => 'error', 'text' => 'Ошибка добавления отзыва'];
+    }
 
     redirect('/otzyvy.php');
 } else {
--- otzyvy.php
+++ otzyvy.php
@@ -110,10 +110,42 @@
                 <a class='action_button actbtn-w' href='registration.php?referer=<?= htmlspecialchars($_SERVER["REQUEST_URI"]) ?>'>Зарегистрироваться</a>
             </div>
         <?php } ?>
-
     </main>
     <?php require_once 'includes/components/footer.php'; ?>
     <script src="/js/textarea.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            <?php
+            $message = $_SESSION['message'] ?? null;
+            unset($_SESSION['message']); ?>
+            <?php if ($message): ?>
+                const message = <?= json_encode($message); ?>;
+                const messageHTML = `
+                <div class='message-block ${message.type}'>
+                    <div class='message'>
+                        <svg class='message-icon' viewBox='0 0 13 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                            <path fill-rule='evenodd' clip-rule='evenodd' d='M6.5 12.5C7.28793 12.5 8.06815 12.3448 8.7961 12.0433C9.52405 11.7417 10.1855 11.2998 10.7426 10.7426C11.2998 10.1855 11.7417 9.52405 12.0433 8.7961C12.3448 8.06815 12.5 7.28793 12.5 6.5C12.5 5.71207 12.3448 4.93185 12.0433 4.2039C11.7417 3.47595 11.2998 2.81451 10.7426 2.25736C10.1855 1.70021 9.52405 1.25825 8.7961 0.956723C8.06815 0.655195 7.28793 0.5 6.5 0.5C4.9087 0.5 3.38258 1.13214 2.25736 2.25736C1.13214 3.38258 0.5 4.9087 0.5 6.5C0.5 8.0913 1.13214 9.61742 2.25736 10.7426C3.38258 11.8679 4.9087 12.5 6.5 12.5ZM6.34533 8.92667L9.67867 4.92667L8.65467 4.07333L5.788 7.51267L4.30467 6.02867L3.362 6.97133L5.362 8.97133L5.878 9.48733L6.34533 8.92667Z'
+                                    fill='currentColor' />
+                            </svg>
+                            <p class='message-text'>${message.text}</p>
+                        </div>
+                    </div>
+                </div> `;
+
+                const mainElement = document.querySelector('main');
+                mainElement.insertAdjacentHTML('afterbegin', messageHTML);
+                const messageBlock = mainElement.querySelector('.message-block');
+
+                messageBlock.classList.add('show');
+                setTimeout(() => {
+                    messageBlock.classList.remove('show');
+                    setTimeout(() => {
+                        mainElement.removeChild(messageBlock);
+                    }, 300);
+                }, 4000);
+            <?php endif; ?>
+        });
+    </script>
 </body>
 
 </html>
