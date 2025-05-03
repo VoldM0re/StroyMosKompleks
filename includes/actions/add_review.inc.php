38a139972cdb94d0bb133af9e563bc199173e857
unknown <dadasik89@gmail.com>
2025-05-03 23:36:25 +0300

    fix: Исправлен тип сообщения при отправке отызва

--- includes/actions/add_review.inc.php
+++ includes/actions/add_review.inc.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         $stmt = $pdo->prepare("INSERT INTO `reviews` (`user_id`, `review_text`) VALUES (:user_id, :review_text);");
         $stmt->execute([':review_text' => $review_text, ':user_id' => $_SESSION['user']['id']]);
-        setMessage('Ваш отзыв отправлен на модерацию');
+        setMessage('Ваш отзыв отправлен на модерацию', 'success');
     } catch (\Throwable $th) {
         setMessage('Ошибка добавления отзыва');
     }
